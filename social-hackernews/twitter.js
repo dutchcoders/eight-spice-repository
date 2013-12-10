810a2ac0f710aa99b5320168adacc491b453a2b8
Your full name <remco@red5.nl>
2013-12-10 16:32:29 +0100

    cleanup

--- social-hackernews/twitter.js
+++ social-hackernews/twitter.js
@@ -24,19 +24,21 @@ function loadScript(url, callback) {
 
 
 loadScript("https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function ($) {
-        var cssLink = $("<link>");
-        cssLink.insertBefore($('link:first')); // prepend allows overrule
-        cssLink.attr({
-            rel: "stylesheet",
-            type: "text/css",
-            href: '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'
-        });
-        
+    var cssLink = $("<link>");
+    cssLink.insertBefore($('link:first')); // prepend allows overrule
+    cssLink.attr({
+        rel: "stylesheet",
+        type: "text/css",
+        href: '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'
+    });
+
+    $(document).ready(function() {
         $('.title').each(function(i, elem) {
-            $("<a href='https://twitter.com/share?url="+$(this).attr('href')+"&text="+$(this).text()+"' class='twitter-share-button' data-lang='en'>Tweet</a>")
+            $("<a href='https://twitter.com/share?url="+$(this).attr('href')+"&text="+$(this).text()+"' class='twitter-share-button' data-lang='en'>Tweet</a>").appendTo($(this));
         });
         
         !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+    });
  });
 })();
 
