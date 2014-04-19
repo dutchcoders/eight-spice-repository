9aced2e6479017598e75a3328a4073eecd1f4e3b
Djaimy Hoogerwerf <djaimy@marketingbrigade.nl>
2014-04-19 12:37:28 +0200

    debug
    .

--- nu.nl/ext.js
+++ nu.nl/ext.js
@@ -1,4 +1,4 @@
-(function() {
+(function() {    
     function loadScript(url, callback) {
 		var script = document.createElement("script")
 		script.type = "text/javascript";
@@ -22,9 +22,11 @@
 		document.getElementsByTagName("head")[0].appendChild(script);
 	}
         loadScript("https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function ($) {
-            $('*[data-vr-contentbox] a').each(function(index, value){
-                console.log(index);
-                console.log(value);
+            $(document).ready(function(){
+                $('div.list ul li[data-vr-contentbox] a').each(function(index, value){
+                    console.log(index);
+                    console.log(value);
+                });
             });
         });
 })();
